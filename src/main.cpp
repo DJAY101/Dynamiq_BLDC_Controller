43cf01ea5feabe22c65f367b6074d491bfaf6175
David Jin <55177986+DJAY101@users.noreply.github.com>
2025-07-24 13:11:40 +1000

    Added GNU License

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 //////// Written by David J ////////
 /////// ESP-32-S3 Wroom 1 Based BLDC Controller /////
 
+/*
+    The following program runs a simple brushless motor using sinusoidal control
+    Copyright (C) 2025 Dawei Jin
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
 #include <stdlib.h>
 
 #include "math.h"
